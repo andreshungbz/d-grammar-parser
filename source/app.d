767746bfefbd50f20316fba4c9ca385f55c9983c
Andres Hung <andres.hung@outlook.com>
2025-09-13 17:12:47 -0600

    Add main program infinite loop

--- source/app.d
+++ source/app.d
@@ -1,7 +1,24 @@
+// Main program
+
+import std.stdio;
+import std.string;
 import printer;
 
 void main()
 {
 	printIntroduction();
 	printGrammar();
+
+	// continually prompt for input
+	while (true)
+	{
+		write("Enter input ('END' to exit): ");
+		string input = strip(readln());
+
+		// break condition on "END"
+		if (input == "END")
+		{
+			break;
+		}
+	}
 }
