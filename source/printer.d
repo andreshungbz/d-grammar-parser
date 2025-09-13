01fd1d64652ea4213e8037b63e8f6004e18a69de
Andres Hung <andres.hung@outlook.com>
2025-09-13 17:26:29 -0600

    Refactor to use associative arrays and make rules global for later

--- source/printer.d
+++ source/printer.d
@@ -5,17 +5,26 @@ module printer;
 import std.format;
 import std.stdio;
 
+// grammar
+string[string] rules = [
+  "<graph>": "HI <draw> BYE",
+  "<draw>": "<action> | <action> ; <draw>",
+  "<action>": "bar <x><y>,<y> | line <x><y>,<x><y> | fill <x><y>",
+  "<x>": "A | B | C | D | E",
+  "<y>": "1 | 2 | 3 | 4 | 5"
+];
+
 /// printIntroduction displays informatino about the program and project.
 void printIntroduction()
 {
-  string[][] programIntroEntries = [
-    ["[d-grammar-parser]", "Simple lexical/syntax analyzer written in D"],
-    ["[GitHub]", "https://github.com/andreshungbz/d-grammar-parser"]
+  string[string] programIntroEntries = [
+    "[d-grammar-parser]": "Simple lexical/syntax analyzer written in D",
+    "[GitHub]": "https://github.com/andreshungbz/d-grammar-parser"
   ];
 
-  foreach (string[] entry; programIntroEntries)
+  foreach (entry, description; programIntroEntries)
   {
-    writeln(format("%-20s %-50s", entry[0], entry[1]));
+    writeln(format("%-20s %-50s", entry, description));
   }
 
   writeln();
@@ -24,23 +33,14 @@ void printIntroduction()
 /// printGrammar displays the BNF grammar rules according to the program specifications.
 void printGrammar()
 {
-  string[] headers = ["[Non-Terminal]", "-->", "[Derivation]"];
-  string[][] rules = [
-    ["<graph>", "-->", "HI <draw> BYE"],
-    ["<draw>", "-->", "<action> | <action> ; <draw>"],
-    ["<action>", "-->", "bar <x><y>,<y> | line <x><y>,<x><y> | fill <x><y>"],
-    ["<x>", "-->", "A | B | C | D | E"],
-    ["<y>", "-->", "1 | 2 | 3 | 4 | 5"]
-  ];
-
   // print headers
   writefln("[BNF/Context-free Grammar]");
-  writeln(format("%-15s %-5s %-50s", headers[0], headers[1], headers[2]));
+  writeln(format("%-15s %-5s %-50s", "[Non-Terminal]", "-->", "[Derivation]"));
 
   // print rules
-  foreach (string[] rule; rules)
+  foreach (nonTerminal, derivation; rules)
   {
-    writeln(format("%-15s %-5s %-50s", rule[0], rule[1], rule[2]));
+    writeln(format("%-15s %-5s %-50s", nonTerminal, "-->", derivation));
   }
 
   writeln();
