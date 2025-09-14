e81636e45f94d4d83e420c9db088de354f28cc83
Andres Hung <andres.hung@outlook.com>
2025-09-13 23:30:35 -0600

    Start on a Token struct

---
+++ source/syntax/token.d
@@ -0,0 +1,12 @@
+module syntax.token;
+
+import syntax.symbols;
+import std.conv : to;
+
+/// Represents a single token produced by the lexer.
+struct Token
+{
+  Terminal kind;
+  string lexeme;
+  size_t pos;
+}
